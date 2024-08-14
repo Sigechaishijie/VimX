05564e7639dbfb54b0cbbc35d5f55e839179f622
思哥拆世界 <albert.peng0826@foxmail.com>
2024-08-14 01:16:02 +0000

    add lua/plugins/which-key.lua.
    Signed-off-by: 思哥拆世界 <albert.peng0826@foxmail.com>

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    config = function(_, opts)
+      local wk = require("which-key")
+      wk.setup(opts)
+      local keymaps = {
+        -- TODO:
+      }
+      wk.register(keymaps)
+    end,
+    opts = {
+      key_labels = {
+        ["<leader>"] = "SPC",
+        ["<tab>"] = "TAB",
+      },
+    },
+  },
+}
